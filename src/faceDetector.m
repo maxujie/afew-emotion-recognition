dae7a23a6ab3aa7a9301749347366919873bd201
maxujie <maxujie14@gmail.com>
2016-11-21 19:21:56 +0800

    add matlab face detector

--- src/faceDetector.m
+++ src/faceDetector.m
@@ -3,4 +3,15 @@ function [faces, frames] = faceDetector(video)
 faces = {};
 frames = [];
 
+% 一个使用 Matlab 自带人脸检测的例子
+face_detector = vision.CascadeObjectDetector();
+for i = 1:video.nrFramesTotal
+    img = video.frames(i).cdata;
+    bbox  = step(face_detector, img);
+    for j = 1:size(bbox, 1)
+        faces{end+1} = img(bbox(1, 2):(bbox(1, 2)+bbox(1, 3)), (bbox(1, 1):(bbox(1, 1)+bbox(1, 4))));
+        frames(end+1) = i;
+    end
+end
+
 end
