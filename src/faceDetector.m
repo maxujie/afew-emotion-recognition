9b3b062dba6f9f7db97d8f08d02a69a88c7d7256
maxujie <maxujie14@gmail.com>
2016-11-21 19:23:04 +0800

    add matlab face detector

--- src/faceDetector.m
+++ src/faceDetector.m
@@ -9,7 +9,7 @@ for i = 1:video.nrFramesTotal
     img = video.frames(i).cdata;
     bbox  = step(face_detector, img);
     for j = 1:size(bbox, 1)
-        faces{end+1} = img(bbox(1, 2):(bbox(1, 2)+bbox(1, 3)), (bbox(1, 1):(bbox(1, 1)+bbox(1, 4))));
+        faces{end+1} = img(bbox(j, 2):(bbox(j, 2)+bbox(j, 3)), (bbox(j, 1):(bbox(j, 1)+bbox(j, 3))));
         frames(end+1) = i;
     end
 end
